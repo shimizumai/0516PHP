7353d1129ef21b3a0c8b111efc04d54a0086f792
mai <shimizu12mai24@gmail.com>
2021-05-24 12:37:42 +0000

    課題修正

--- 04practice.php
+++ 04practice.php
@@ -25,24 +25,24 @@ for ($i = 0; $i <= 100; $i++) {
 echo $total."\n";
 
 $name = "mai";
-if ($name == "mai") {
- echo "私は" . $name . "です" ."\n";
+if ($name === "mai") {
+    echo "私は" . $name . "です" ."\n";
 } else {
-    echo "あなたの名前ではありません";
+    echo $name . "ではありません";
 }
 
 $start = 1;
 $end = 10000;
 $total = 0;
-for($i = $start; $i <=$end; $i++){
+for($i = $start; $i <= $end; $i++){
     $total += $i;
 }
 echo $total ."\n"; //50005000
 
- $fruits = array("apple", "orange", "lemon", "meron", "momo");
- foreach($fruits as $fruit){
-     echo "要素は" . $fruit;
-     echo "\n";
+$fruits = array("apple", "orange", "lemon", "meron", "momo");
+foreach($fruits as $fruit){
+    echo "要素は" . $fruit;
+    echo "\n";
 }
 
 //for文の始めの値を定義する
@@ -50,11 +50,11 @@ $start = 1;
 // for文の終わりの値を定義する
 $end = 100;
 
-for($i = $start; $i <=$end; $i++){
+for($i = $start; $i <= $end; $i++){
 
   // 5で割り切れたら{}内を実行する
-  if($i % 5 == 0){
-    echo $i."\n";
+  if($i % 5 === 0){
+    echo $i . "\n";
   }
 }
 
